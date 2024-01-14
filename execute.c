d5ef6ba622a0820dd52b3910d6942390a83dee84
jan_ice17 <judysjewel17@gmail.com>
2024-01-13 19:53:33 -0500

    Handle PATH and fork after access successful

--- execute.c
+++ execute.c
@@ -1,6 +1,5 @@
 #include "shell.h"
 
-
 /**
  * Executes a program specified by the file path with the given arguments.
  * 
@@ -8,8 +7,44 @@
  * @args: The arguments to be passed to the program.
  * @program_name: The name of the program.
  */
+
 void _execute(const char *file_path, char **const args, const char *program_name) {
-    pid_t jantee_pid = fork();
+    pid_t jantee_pid;
+    char *full_path = NULL;
+
+    if (file_path[0] != '/') {
+        char *path_env = getenv("PATH");
+        char *path_copy = _strdup(path_env);
+        char *token = _strtok(path_copy, ":");
+
+        while (token != NULL && full_path == NULL) {
+            full_path = malloc(_strlen(token) + _strlen(file_path) + 2);
+            _strcpy(full_path, token);
+            _strcat(full_path, "/");
+            _strcat(full_path, file_path);
+
+            if (access(full_path, X_OK) == -1) {
+                free(full_path);
+                full_path = NULL;
+            }
+
+            token = _strtok(NULL, ":");
+        }
+
+        free(path_copy);
+
+        if (full_path == NULL) {
+            perror(program_name);
+            return;
+        }
+
+        file_path = full_path;
+    } else if (access(file_path, X_OK) == -1) {
+        perror(program_name);
+        return;
+    }
+
+    jantee_pid = fork();
 
     if (jantee_pid == -1) {
         perror(program_name);
@@ -24,6 +59,6 @@ void _execute(const char *file_path, char **const args, const char *program_name
     } else {
         waitpid(jantee_pid, NULL, 0);
     }
+
+    free(full_path);
 }
-
-
--- strings.c
+++ strings.c
@@ -1,66 +1,6 @@
 #include "shell.h"
 
 /**
- * _strchr - Locates a character in a string.
- * @s: The string to search.
- * @c: The character to locate.
- *
- * Return: Pointer to first occurrence of character c in the string s
- */
-char *_strchr(const char *s, char c) {
-    if (s == NULL) {
-        return NULL;
-    }
-
-    while (*s) {
-        if (*s == c)
-            return (char *)s;
-        s++;
-    }
-    return NULL;
-}
-
-
-/**
- * _strspn - Gets length of a prefix substring.
- * @s: The string to be searched.
- * @accept: The string containing the characters to match.
- *
- * Return: No. of bytes in initial segment of s.
- */
-size_t _strspn(const char *s, const char *accept) {
-    size_t count = 0;
-    if (s == NULL || accept == NULL) {
-        return 0;
-    }
-
-    while (*s && _strchr(accept, *s++))
-        count++;
-
-    return count;
-}
-
-/**
- * _strcspn - Gets length of a prefix substring.
- * @s: The string to be searched.
- * @reject: The string containing the characters to avoid.
- *
- * Return: Num. of bytes in initial segment of s which consist only
- *         of bytes NOT in reject, or 0 if s or reject is NULL.
- */
-size_t _strcspn(const char *s, const char *reject) {
-    size_t c = 0;
-    if (s == NULL || reject == NULL) {
-        return 0;
-    }
-
-    while (*s && !_strchr(reject, *s++))
-        c++;
-
-    return c;
-}
-
-/**
  * _strlen - Calculate length of string.
  * @s: The string to calculate the length of.
  *
@@ -76,4 +16,69 @@ size_t _strlen(const char *s) {
         length++;
     }
     return length;
+}
+
+
+/**
+ * _strdup - Duplicates a string.
+ * @s: The string to be duplicated.
+ *
+ * Return: Pointer to the duplicated string, or NULL if insufficient memory
+ *         was available.
+ */
+char *_strdup(const char *s) {
+    char *dup;
+    size_t len, i;
+
+    if (s == NULL) {
+        return NULL;
+    }
+
+    len = _strlen(s);
+
+    dup = malloc(sizeof(char) * (len + 1));
+
+    if (dup == NULL) {
+        return NULL;
+    }
+
+    for (i = 0; i < len; i++) {
+        dup[i] = s[i];
+    }
+
+    dup[i] = '\0';
+
+    return dup;
+}
+
+
+/**
+ * _strcat - Concatenates two strings.
+ * @dest: The string to be appended to.
+ * @src: The string to append.
+ * 
+ */
+void _strcat(char *dest, const char *src) {
+    int i, j;
+    for (i = 0; dest[i] != '\0'; i++);
+    for (j = 0; src[j] != '\0'; j++) {
+        dest[i] = src[j];
+        i++;
+    }
+    dest[i] = '\0';
+}
+
+
+/**
+ * _strcpy - Copies a string.
+ * @dest: The destination string.
+ * @src: The source string.
+ * 
+ */
+void _strcpy(char *dest, char *src) {
+    int i;
+    for (i = 0; src[i] != '\0'; i++) {
+        dest[i] = src[i];
+    }
+    dest[i] = '\0';
 }
